3becbff812a0b390fef1355aac15cfcb39fdc937
breezyairdropp <breezyairdrop@gmail.com>
2024-06-02 16:33:46 +0100

    Update leadsGeneration.js

--- leadsGeneration.js
+++ leadsGeneration.js
@@ -89,10 +89,18 @@ let parsedData = 'auto repair shop austin'
 		puppeteer.use(stealthPlugin())
 
 		const browser = await puppeteer.launch({
-			headless: true,
-			args: ['--no-sandbox'],
-			executablePath: ''
-		})
+        // headless: true,
+        args: [
+            "--disable-setuid-sandbox",
+            "--no-sandbox",
+            "--single-process",
+            "--no-zygote",
+          ],
+          executablePath:
+            process.env.NODE_ENV === "production"
+              ? process.env.PUPPETEER_EXECUTABLE_PATH
+              : puppeteer.executablePath(),
+    });
 
 		const page = await browser.newPage()
 		const query = parsedData
