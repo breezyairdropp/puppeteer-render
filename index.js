abd2da80d83e03db234aea0afb69b7eb6874b4ca
breezyairdropp <breezyairdrop@gmail.com>
2024-07-09 04:20:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const { scrapeLogic } = require("./scrapeLogic");
 const { googleLogic } = require("./googleLogic");
 const { leadsGeneration } = require("./leadsGeneration");
 const { leadsGenerationPro } = require("./leadsGenerationPro");
-
+const path = require("path");
 
 
 const app = express();
+app.use(express.static(__dirname + "/out"));
 
 const PORT = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+    app.render('index', function (err, html) {
+        if (err) console.log(err);
+        res.send(html)
+    });
+  })
 
 app.get("/scrape", (req, res) => {
   scrapeLogic(res);
@@ -26,9 +34,9 @@ app.get("/leadspro", (req, res) => {
   leadsGenerationPro(req, res);
 });
 
-app.get("/", (req, res) => {
-  res.send("Render Puppeteer server is up and running!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Render Puppeteer server is up and running!");
+// });
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
